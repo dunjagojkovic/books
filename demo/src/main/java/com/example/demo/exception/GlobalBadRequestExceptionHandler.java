e3afba1887fb4bcbf31f908bf92461d3e4fa7dc2
Dunja Gojković <gojkovic.ra37.2018@uns.ac.rs>
2024-09-10 09:57:43 +0200

    added multiple global exception handler classes

--- demo/src/main/java/com/example/demo/exception/GlobalBadRequestExceptionHandler.java
+++ demo/src/main/java/com/example/demo/exception/GlobalBadRequestExceptionHandler.java
@@ -8,12 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.sql.SQLIntegrityConstraintViolationException;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> handleBookNotFoundException(EntityNotFoundException ex){
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
-    }
+public class GlobalBadRequestExceptionHandler {
 
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
     public ResponseEntity<String> handleDuplicatedBookTitleExistsException(BookTitleAlreadyExistsException ex){
@@ -24,6 +19,4 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleGlobalException(Exception ex) {
         return new ResponseEntity<>("An error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
-
 }
